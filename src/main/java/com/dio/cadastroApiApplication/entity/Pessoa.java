afc60578f715090b86911936235b56552e4d336f
Wiltzl <wiltalibanzl@yahoo.com.br>
2021-08-20 14:23:28 -0300

    adiciona procfile

---
+++ src/main/java/com/dio/cadastroApiApplication/entity/Pessoa.java
@@ -0,0 +1,4 @@
+package com.dio.cadastroApiApplication.entity;
+
+public class Pessoa {
+}
--- src/test/java/com/dio/cadastroApiApplication/CadastroApiApplicationTests.java
+++ src/test/java/com/dio/cadastroApiApplication/CadastroApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CadastroApiApplicationTests {
+class cadastroApiApplicationTests {
 
 	@Test
 	void contextLoads() {
